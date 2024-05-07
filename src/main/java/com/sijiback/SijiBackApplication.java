160319da76a3d7fdd9379750912047a3b17b3d2e
Cornelius <2286366284@qq.com>
2024-05-07 10:28:01 +0800

    first commit

---
+++ src/main/java/com/sijiback/SijiBackApplication.java
@@ -0,0 +1,13 @@
+package com.sijiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SijiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SijiBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sijiback/SijiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sijiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SijiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
